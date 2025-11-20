c8bf3b68b316904bba3965c252524a53447840f6
Achmad Atha <achmadatha@mhs.usk.ac.id>
2025-11-20 22:21:33 +0700

    Save local changes before switching branch

---
+++ app/components/LoopingText.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
+type Props = {
+    phrases: string[];
+};
+
+
+const LoopingText = ({ phrases }: Props) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % phrases.length);
+    }, 4700); // ganti setiap 4 detik
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <h2
+      className="text-4xl animate-fade-in transition-opacity duration-700 ease-in-out"
+      style={{
+        fontFamily: "var(--font-testsohne)",
+        fontWeight: 800,
+      }}
+    >
+      {phrases[index]}
+    </h2>
+  );
+};
+
+export default LoopingText;
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Membuat keluaran statis untuk export
-  basePath: '/Portfolio', // Sesuaikan dengan nama repository Anda
-  assetPrefix: '/Portfolio/', // Agar asset diarahkan ke URL GitHub Pages yang benar
   reactStrictMode: true,
+  output: "export",
 };
 
 export default nextConfig;
